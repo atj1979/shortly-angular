9ae1470506bc809a048ad6fb1c6ed0deaa30b71f
atj1979 <atj1979@gmail.com>
2015-03-06 15:25:34 -0800

    Start making view and controller for links.
    Start making view for links
    Start building controller for links
    Put comments in shorten controller
    Add third argument in userModel for authentication.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,16 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = {};
+  // WHEN CONTROLLER LOADS, CALL GETLINKS PROPERTY ($SCOPE.GETLINKS())
+  $scope.signin = function () {
+  Auth.signin($scope.user)
+    .then(function (token) {
+      $window.localStorage.setItem('com.shortly', token);
+      $location.path('/links');
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  };
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,3 +3,8 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
 });
+
+
+// ShortenController should have a link property on t
+// ShortenController should have a addLink method on the $scope
+// ShortenController should be able to create new links with addLink()
