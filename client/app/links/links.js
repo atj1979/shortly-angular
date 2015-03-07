5bde83239d4e8d86937a9a821b53be4bb788adbe
atj1979 <atj1979@gmail.com>
2015-03-06 17:14:06 -0800

    Angular routes work

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,14 +3,24 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links) {
   $scope.data = {};
   // WHEN CONTROLLER LOADS, CALL GETLINKS PROPERTY ($SCOPE.GETLINKS())
-  $scope.signin = function () {
-  Auth.signin($scope.user)
-    .then(function (token) {
-      $window.localStorage.setItem('com.shortly', token);
-      $location.path('/links');
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+  // LinksController should have a getLinks method on the $scope
+  $scope.getLinks = function () {
+    // send a get request (to server) to get all the links
+    $http.get('/api/links').
+      success(function(data, status, headers, config) {
+        console.log(data);
+      }).
+      error(function(data, status, headers, config) {
+        console.error("Failed GET links request");
+      });
+    // when it comes back, display the links through the view
+  // Auth.signin($scope.user)
+    // .then(function (token) {
+      // $window.localStorage.setItem('com.shortly', token);
+    //   $location.path('/links');
+    // })
+    // .catch(function (error) {
+    //   console.error(error);
+  //  });
   };
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,7 +1,30 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+  $scope.link = {};
+
+  $scope.addLink = function () {
+    $http.post('/api/links', { url:link.url }).
+      success(function(data, status, headers, config) {
+        console.log(data);
+      }).
+      error(function(data, status, headers, config) {
+        console.error("Failed POST links request");
+      });
+    }
+
+
+
+  // something($scope.link)
+  //   Auth.signin($scope.user)
+  //     .then(function (token) {
+  //       $window.localStorage.setItem('com.shortly', token);
+  //       $location.path('/links');
+  //     })
+  //   .catch(function (error) {
+  //     console.error(error);
+  //   });
+  // };
 });
 
 
